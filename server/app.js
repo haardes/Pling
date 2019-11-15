2c99230d117f7d3158b5f78d77ef4e0d1dae352b
Fredrik Haarde <32198607+haardes@users.noreply.github.com>
2019-11-15 14:40:05 +0100

    Production of angular added to server

--- server/app.js
+++ server/app.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const PORT = process.env.PORT || 8080;
-const app = express();
-const api = require('./routes/api');
+const PORT = process.env.PORT || 8080
+const app = express()
+const api = require('./routes/api')
 
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.json())
+app.use(cors())
 
-app.use('/api', api);
+app.use('/api', api)
 
 app.get('/', (req, res) => {
-    res.send('Hello from the server');
-    res.end();
-});
+  res.send("Hello from the server");
+  res.end();
+})
 
 app.listen(PORT, () => {
-    console.log('Server listening on port: ' + PORT);
-});
+  console.log('Server listening on port: ' + PORT)
+})
